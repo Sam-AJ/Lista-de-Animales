9989da274d2d6569d4270bdbe9cee6c54c2e89eb
Samuel Jara <spjara.aravena@gmail.com>
2022-02-14 22:55:16 -0300

    Version de Entrega

--- assets/js/propietario.js
+++ assets/js/propietario.js
@@ -7,8 +7,7 @@ class Propietario{
 
     datosPropietario(){
         return `
-        El nombre del dueño es: ${this._nombre}. El domicilio es: ${this._direccion} y el número telefónico de contacto: ${this._telefono}<br>
-        El tipo de animal es un: ${this.tipo} mientras que el nombre de la mascota es:  ${this.nombre} y la enfermedad es: ${this.enfermedad}
+        El nombre del dueño es: ${this._nombre}. El domicilio es: ${this._direccion} y el número telefónico de contacto: ${this._telefono}
         `
     }
 };
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,34 @@
 import Mascota from "./mascota.js";
 
+let listado = [];
+
 $(() => {
     
-})
+    let listar = () => {
+        $("#resultado ul").html("");
+        listado.forEach((item) => {
+            $("#resultado ul").append(`
+                <li>${item.datosPropietario()}</li>
+                <li>El tipo de animal es un: ${item.tipo} mientras que el nombre de la mascota es:  ${item.nombreMascota} y la enfermedad es: ${item.enfermedad}</li>
+                <br>
+            `)
+        })
+    }
+    
+    $("form").submit(evento => {
+        evento.preventDefault();
+
+        let nombre = $("#propietario").val();
+        let direccion = $("#direccion").val();
+        let telefono = $("#telefono").val();
+        let tipo = $("#tipo").val();
+        let nombreMascota = $("#nombreMascota").val();
+        let enfermedad = $("#enfermedad").val();
+        
+        let item = new Mascota(nombre, direccion, telefono, tipo, nombreMascota, enfermedad);
+
+        listado.push(item);
+        $("form")[0].reset();
+        listar();
+    })
+});
